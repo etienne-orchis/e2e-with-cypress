cc9458dc3b81266c39f514cc4faebd1fe019c6fb
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-04-01 15:24:03 +0300

    adding more edge cases

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,32 @@
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   router = inject(Router);
+  email = '';
+  password = '';
+  errorMessage = '';
   login() {
-    console.log('User logged in');
-    this.router.navigate(['/dashboard']);
+    if (!this.email || !this.password) {
+      this.errorMessage = 'Email and password are required';
+      return;
+    }
+
+    if (
+      this.email === 'testuser@example.com' &&
+      this.password === 'password123'
+    ) {
+      console.log('User logged in successfully');
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.errorMessage = 'Invalid email or password';
+    }
   }
 }
